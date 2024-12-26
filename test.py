9cab2312cd5be27e32ec30a85319dd2a8ab8a54d
Snaxx <95628737+snaxx78@users.noreply.github.com>
2024-12-26 19:00:00 +0100

    feat : added leaderboard

--- test.py
+++ test.py
@@ -1,10 +1,56 @@
 import discord
+import sqlite3
 from discord.ext import commands
 
 TOKEN = "MTMyMTgzNDI4NzExNjMyMDgzOA.G0F6U7.mfTzYHAAjCuqKyaIHltNgHaCis5UoqxFldDrbw"
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
+intents.message_content = True
+intents.typing = False
+intents.presences = False
+
+# Connexion à la base de données
+def get_leaderboard():
+    conn = sqlite3.connect("rootme_data.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT username, points FROM user_data ORDER BY points DESC")
+    leaderboard = cursor.fetchall()  # Récupère tous les utilisateurs triés par points
+    conn.close()
+    return leaderboard
+
+# Fonction pour déterminer le commentaire à afficher
+def get_commentary(points):
+    if points < 1000:
+        return "- encore vachement loin des 4000"
+    elif points < 2000:
+        return "- tu commences à t'approcher des 4000 !"
+    elif points < 3000:
+        return "- presque au top, encore un peu d'effort !"
+    elif points < 4000:
+        return "- ça y est, tu es dans la course !"
+    else:
+        return "- Top niveau ! 💪"
+
+# Commande !leaderboard
+@bot.command(name="leaderboard")
+async def leaderboard(ctx):
+    leaderboard = get_leaderboard()
+    
+    if not leaderboard:
+        await ctx.send("Il n'y a pas de données dans la base.")
+        return
+    
+    # Création du message à envoyer
+    message = "🏆 **Leaderboard des utilisateurs** 🏆\n"
+    message += "\n".join([
+    f"{i+1}. **{user[0]}** - {user[1]} points  {get_commentary(user[1]) if user[0] == 'Mac-812606' else ''}"
+    for i, user in enumerate(leaderboard)
+])
+
+    
+    # Envoi du message
+    await ctx.send(message)
 
 @bot.event
 async def on_ready():
@@ -24,3 +70,5 @@ async def on_ready():
         print("Channel introuvable !")
 
 bot.run(TOKEN)
+
+
